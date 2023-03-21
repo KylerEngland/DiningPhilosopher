4533ec27e6d1b1f4e152e1ba3b5ed67721813a05
danielnichols91 <danielnichols0912@gmail.com>
2023-03-21 08:01:16 -0400

    Setup

--- DiningPhilosophers/src/Main/Frame.java
+++ DiningPhilosophers/src/Main/Frame.java
@@ -1,10 +1,20 @@
 package Main;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class Frame extends JFrame{
     public Frame(){
         this.setTitle("Dinner");
-
+        this.setSize(new Dimension(1024, 640));
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false); 
+		this.setLayout(new BorderLayout());
+		ImageIcon logo = new ImageIcon("../spaghettiLogo.png");
+		this.setIconImage(logo.getImage()); 
+        this.setVisible(true);
     }
 }
