05a25f2470131d99e198e84e276e548f7c4abe51
danielnichols91 <danielnichols0912@gmail.com>
2023-03-22 21:28:39 -0400

    Plates Placed

--- DiningPhilosophers/src/Main/Panel.java
+++ DiningPhilosophers/src/Main/Panel.java
@@ -15,6 +15,11 @@ public class Panel extends JPanel {
             Graphics2D g2 = (Graphics2D)g;
             g2.setStroke(new BasicStroke(3));
             g2.setColor(Color.BLACK);
-            g2.drawOval(200, 200, 200, 200);
+            g2.drawOval(200, 200, 300, 300);
+            g2.drawOval(310, 210, 75, 75);
+            g2.drawOval(410, 290, 75, 75);
+            g2.drawOval(370, 400, 75, 75);
+            g2.drawOval(260, 400, 75, 75);
+            g2.drawOval(220, 290, 75, 75);
         }
 }
